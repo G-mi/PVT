08a9ee52096fcc1f6714dcb9b349157ce76df3e8
Dimitris Mavromatis <mavromatisdimitris@protonmail.com>
2021-04-28 09:20:47 +0200

    iniital commit

---
+++ src/test/java/tennisPartner/TennisPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package tennisPartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
