1e655b109414193049a5e43374ce9870bbf75e68
MarikaK <karlsson.marika@gmail.com>
2021-05-06 12:14:40 +0200

    Frontend

--- lib/main.dart
+++ lib/main.dart
@@ -59,55 +59,89 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  return Scaffold(
+  backgroundColor: Colors.white,
+
+  body: SingleChildScrollView(
+  child: Column(
+  children: <Widget>[
+  Padding(
+  padding: const EdgeInsets.only(top: 60.0),
+  child: Center(
+  child: Container(
+  width: 200,
+  height: 300,
+  /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+  child: Image.asset('images/logo.png')),
+  ),
+  ),
+
+  Padding(
+  padding: const EdgeInsets.only(
+  left: 15.0, right: 15.0, top: 15, bottom: 20),
+      child: ElevatedButton(
+        onPressed: () {  //Navigator.push(
+          // context, MaterialPageRoute(builder: (_) => SignUp()));
+        },
+        style: ElevatedButton.styleFrom(
+            primary: Colors.red,
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+            textStyle: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold)),
+        child: Row (mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text("Sign in  ",style: TextStyle(fontSize: 20)),
+            Icon(Icons.arrow_forward,
+              color: Colors.black,
+              size: 30.0,),
           ],
-        ),
+        ),)
+  ),
+
+
+    Padding(
+      padding: const EdgeInsets.only(
+          left: 15.0, right: 15.0, top: 15, bottom: 20),
+      //padding: EdgeInsets.symmetric(horizontal: 15),
+      child: ElevatedButton(
+        onPressed: () {  //Navigator.push(
+          // context, MaterialPageRoute(builder: (_) => SignUp()));
+        },
+        style: ElevatedButton.styleFrom(
+            primary: Colors.green,
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+            textStyle: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold)),
+        child: Row (mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Sign up    ",style: TextStyle(fontSize: 20)),
+            Icon(Icons.arrow_forward,
+              color: Colors.black,
+              size: 30.0,),
+          ],
+        ),)
+
+
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+
+
+
+  ],
+  ),
+  ),
+  );
   }
-}
+  }
+
+
+
+
+
