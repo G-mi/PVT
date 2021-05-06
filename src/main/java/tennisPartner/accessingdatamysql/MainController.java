7f761edbac97fcfe7472cd0bd74debbae6afcbb9
Dimitris Mavromatis <mavromatisdimitris@protonmail.com>
2021-05-06 11:40:25 +0200

    RestController instead of Controller annotation

--- src/main/java/tennisPartner/accessingdatamysql/MainController.java
+++ src/main/java/tennisPartner/accessingdatamysql/MainController.java
@@ -2,13 +2,9 @@ package tennisPartner.accessingdatamysql;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller // This means that this class is a Controller
+@RestController // This means that this class is a Controller
 // path="/demo" was the default here
 @RequestMapping(path="/tennisPartner") // This means URL's start with /demo (after Application path)
 public class MainController {
