e4913e6ea4e2efec3fa05b2225c627f37529d2bb
MarikaK <karlsson.marika@gmail.com>
2021-05-07 10:12:31 +0200

    started sign in

--- lib/signin.dart
+++ lib/signin.dart
@@ -5,6 +5,21 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(centerTitle: true,
+        title: Text('Sign in'),
+        actions: <Widget>
+      [IconButton(icon: Icon(Icons.arrow_back,
+          color: Colors.white,),
+            onPressed: () {
+           Navigator.pop(context);
+            }
+            )
+        ],
+      ),
+    );
+
     // TODO: implement build
     throw UnimplementedError();
   }
