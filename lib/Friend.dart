3ab90ce3cf8c7d80a3a009bb663bd2a688dd4969
ulfmal <72732399+ulfmal@users.noreply.github.com>
2021-05-10 17:10:56 +0200

    Test added for user

--- lib/Friend.dart
+++ lib/Friend.dart
@@ -1,9 +1,20 @@
 
 
-class Friend{
+class Friend {
   String userName;
 
   Friend({
     this.userName
   });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Friend &&
+              runtimeType == other.runtimeType &&
+              userName == other.userName;
+
+  @override
+  int get hashCode => userName.hashCode;
+
 }
--- lib/User.dart
+++ lib/User.dart
@@ -7,23 +7,26 @@ class User {
   List<Friend> friends;
   List<Match> plannedMatches;
 
-  User({
+  User(
     this.firstName,
     this.lastName,
-    this.userName,
-    this.friends,
-    this.plannedMatches
-});
+    this.userName){
+    getFriends();
+  }
 
   void getFriends(){
     this.friends = debugFriends; //Debug test
+  }
+
+  List<Friend> getFriendsTest(){
+    return this.friends;
   }
 
   String getFullName(){
     return firstName + ' ' + lastName;
   }
 
-  final debugFriends = [
+  List<Friend> debugFriends = [
     new Friend(userName: 'Bengt'),
     new Friend(userName: 'Sara'),
     new Friend(userName: 'Monica')
