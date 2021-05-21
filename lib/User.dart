237afed38e318d250fcc72a2dda35406732f6973
Ruben <rubenchambav@gmail.com>
2021-05-21 12:51:02 +0200

    Fixed some problems

--- lib/User.dart
+++ lib/User.dart
@@ -1,30 +1,24 @@
 import 'Friend.dart';
 
 class User {
-  String firstName;
-  String lastName;
-  String userName;
-  String email;
-  String aboutInfo;
-  String profilePicturePath;
-  String gender;
-  int skillLevel;
-  int age;
-  List<Friend> friends;
-  List<Match> plannedMatches;
+    String firstName;
+    String lastName;
+    String userName;
+    //String email;
+    //String aboutInfo;
+    //String profilePicturePath;
+    //String gender;
+    //int skillLevel;
+    //int age;
+    List<Friend> friends;
+    List<Match> plannedMatches;
 
-  User(
-    this.firstName,
-    this.lastName,
-    this.email,
-    this.aboutInfo,
-    this.gender,
-    this.skillLevel,
-    this.age,
-    this.profilePicturePath,
-    this.userName){
-    getFriends();
-  }
+    User(
+        this.firstName,
+        this.lastName,
+        this.userName){
+        getFriends();
+    }
 
   void getFriends(){
     this.friends = debugFriends; //Debug test
@@ -38,7 +32,7 @@ class User {
     return firstName + ' ' + lastName;
   }
 
-  List<Friend> debugFriends = [
+  final List<Friend> debugFriends = [
     new Friend(userName: 'Bengt'),
     new Friend(userName: 'Sara'),
     new Friend(userName: 'Monica')
