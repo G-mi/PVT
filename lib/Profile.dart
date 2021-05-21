30658bf1c9d5c8abb19d3475bb848bd48b65b388
Ruben <rubenchambav@gmail.com>
2021-05-21 11:47:02 +0200

    Updated the Profile Page

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
-//import 'Homescreen.dart';
+import 'Homescreen.dart';
 
 class Profile extends StatefulWidget{
   Profile({Key key}) : super(key:key);
@@ -14,6 +14,35 @@ class _ProfileState extends State<Profile>{
 
   @override
   Widget build(BuildContext context){
-
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/background.jpg'),
+              fit: BoxFit.cover,
+            )
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: buildAppBar(context),
+        ),
+      ],
+    );
   }
+}
+
+AppBar buildAppBar(BuildContext context){
+  return AppBar(
+    leading: IconButton(
+      icon: Icon(Icons.home),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+      },
+    ),
+    backgroundColor: Colors.deepOrange,
+    centerTitle: true,
+    title: const Text('Username'),
+  );
 }
