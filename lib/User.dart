b463ff2017a15df248c175fffebf52ba28d796c5
ulfmal <72732399+ulfmal@users.noreply.github.com>
2021-05-26 03:07:57 +0200

    User update, new "full" constructor and json factory

--- lib/User.dart
+++ lib/User.dart
@@ -4,27 +4,38 @@ class User {
   String firstName;
   String lastName;
   String userName;
-
-  //String email;
-  //String aboutInfo;
-  //String profilePicturePath;
-  //String gender;
-  //int skillLevel;
+  String email;
+  int skillLevel;
   int age;
   List<Friend> friends;
   List<Match> plannedMatches;
+  String profilePicturePath;
 
-  User(this.firstName,
+  //String aboutInfo;
+  //String gender;
+
+
+  User.namesOnly(
+      this.firstName,
       this.lastName,
       this.userName) {
     getFriends();
   }
 
-  User.namesOnly(
+  User(
       this.firstName,
       this.lastName,
       this.userName,
       this.age);
+
+  User.full(
+      this.firstName,
+      this.lastName,
+      this.userName,
+      this.email,
+      this.age,
+      this.skillLevel,
+      );
 
   void getFriends(){
     this.friends = debugFriends; //Debug test
@@ -46,9 +57,19 @@ class User {
 
     //TODO: Complete with all data that is to be loaded
   factory User.fromJson(Map<String, dynamic> json){
-    return User(
+    return User.namesOnly(
         json['firstName'],
         json['lastName'],
         json['userName']);
+  }
+
+  factory User.fromJsonFull(Map<String, dynamic> json){
+    return User.full(
+        json['firstName'],
+        json['lastName'],
+        json['userName'],
+        json['email'],
+        json['age'],
+        json['skillevel']);
   }
 }
