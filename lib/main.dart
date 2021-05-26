f04d02543cb0ac9564177604f263fdd9b26b18a5
Dimitris Mavromatis <mavromatisdimitris@protonmail.com>
2021-05-26 20:10:49 +0200

    Added some lunch code for google maps on main.dark and upadated relevant dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'startscreen.dart';
 import 'signin.dart';
 import 'Buttons.dart';
-
 
 void main() {
   runApp(
@@ -19,4 +19,27 @@ void main() {
       },
     ),
   );
+}
+
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  static const _initialCameraPosition = CameraPosition(
+    target: LatLng(59.334591, 18.063240),
+    zoom: 11.5,
+  ); // Camera Position
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: _initialCameraPosition,
+      ),
+    );
+  }
 }
