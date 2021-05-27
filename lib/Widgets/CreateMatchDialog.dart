c3bc253fa4f3ac0db0618047b63ab13228fcb3ca
Ruben <rubenchambav@gmail.com>
2021-05-27 17:20:47 +0200

    More in the Match Dialog

--- lib/Widgets/CreateMatchDialog.dart
+++ lib/Widgets/CreateMatchDialog.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/Widgets/skillRatingWidget.dart';
+
+import '../NTRPDialog.dart';
 
 class CreateMatchDialog extends StatefulWidget {
   CreateMatchDialog([Key key]) : super(key: key);
@@ -9,6 +12,8 @@ class CreateMatchDialog extends StatefulWidget {
 
 class _CreateMatchDialogState extends State<CreateMatchDialog> {
   int _selectedNrOfPlayers;
+  int _minSkillLevel = 4;
+  int _maxSkillLevel = 4;
 
   @override
   Widget build(BuildContext context) {
@@ -108,11 +113,19 @@ class _CreateMatchDialogState extends State<CreateMatchDialog> {
                       'Date',
                     ),
                   ),
-                  SizedBox(width: 50,),
+                  SizedBox(width: 10,),
+                  Text(
+                    'Number of players',
+                    style: TextStyle(color: Colors.black, fontSize: 10),
+                  ),
+                  SizedBox(width: 5,),
                   Container(
                     child: DropdownButton<int>(
                       value: _selectedNrOfPlayers,
-                      style: TextStyle(color: Colors.white),
+                      dropdownColor: Colors.green,
+                      iconDisabledColor: Colors.green,
+                      iconEnabledColor: Colors.green,
+                      style: TextStyle(color: Colors.black),
                       items: <int>[
                         2,
                         3,
@@ -123,19 +136,95 @@ class _CreateMatchDialogState extends State<CreateMatchDialog> {
                           child: Text('${value}'),
                         );
                       }).toList(),
-                      hint: Text(
-                        'Number of players',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 13,
-                        ),
-                      ),
                       onChanged: (int value) {
                         setState(() {
                           _selectedNrOfPlayers = value;
-
                         });
                       },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5,),
+              Container(
+                width: 280,
+                height: 56,
+                child: Stack(
+                  children: [
+                    SkillRatingWidget(
+                      skillValue: _minSkillLevel,
+                      selectedBall: Icon(
+                        Icons.sports_baseball,
+                        color: Colors.deepOrange,
+                        size: 35,),
+                      unSelectedBall: Icon(
+                        Icons.sports_baseball_outlined,
+                        color: Color.fromRGBO(255, 87, 34, 0.5),
+                        size: 35,),
+                    ),
+                    Positioned(
+                      top: -13,
+                      left: 235,
+                      child: IconButton(
+                        icon: Icon(Icons.info),
+                        iconSize: 27,
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) => NTRPDialog()
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: ClipOval(
+                      child: Container(
+                        color: Colors.deepOrange,
+                        child:  IconButton(
+                          icon: Icon(Icons.remove),
+                          iconSize: 25,
+                          color: Colors.white,
+                          onPressed: () {
+                            setState(() {
+                              if (_minSkillLevel > 1){
+                                _minSkillLevel = _minSkillLevel - 1;
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 5,),
+
+                  SizedBox(width: 5,),
+                  SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: ClipOval(
+                      child: Container(
+                        color: Colors.deepOrange,
+                        child:  IconButton(
+                          icon: Icon(Icons.add),
+                          iconSize: 25,
+                          color: Colors.white,
+                          onPressed: () {
+                            setState(() {
+                              if (_minSkillLevel < 7){
+                                _minSkillLevel = _minSkillLevel + 1;
+                              }
+                            });
+                          },
+                        ),
+                      ),
                     ),
                   ),
                 ],
