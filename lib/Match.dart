aa23c9eed567b69e32e34948f18056561a34ecd6
ulfmal <72732399+ulfmal@users.noreply.github.com>
2021-05-30 00:52:17 +0200

    Update Match

--- lib/Match.dart
+++ lib/Match.dart
@@ -1,10 +1,27 @@
 
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Match{
-  DateTime time;
-  String otherPlayer;
+
+  DateTime date;
+  TimeOfDay startTime;
+  TimeOfDay endTime;
+  int numberOfPlayers;
+  //int freeSlots;
+  int minSkillLevel;
+  int maxSkillLevel;
+  LatLng matchLocation;
+  //List<String> participants;
 
   Match(
-      this.time,
-      this.otherPlayer);
+      {
+        this.date,
+        this.startTime,
+        this.endTime,
+        this.numberOfPlayers,
+        this.minSkillLevel,
+        this.maxSkillLevel,
+        this.matchLocation
+      });
 }
