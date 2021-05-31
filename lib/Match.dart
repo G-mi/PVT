78ba556d8a7bfcca23654558bda685142564486f
MarikaK <karlsson.marika@gmail.com>
2021-05-31 14:36:53 +0200

    match

--- lib/Match.dart
+++ lib/Match.dart
@@ -14,13 +14,39 @@ class Match{
   LatLng matchLocation;
   //List<String> participants;
 
-  Match({
-    this.date,
-    this.startTime,
-    this.endTime,
-    this.numberOfPlayers,
-    this.minSkillLevel,
-    this.maxSkillLevel,
-    this.matchLocation
-  });
+  Match(
+      {
+        this.date,
+        this.startTime,
+        this.endTime,
+        this.numberOfPlayers,
+        this.minSkillLevel,
+        this.maxSkillLevel,
+        this.matchLocation
+      });
+
+  factory Match.fromJsonFull(Map<String, dynamic> json){
+    return Match (
+      date: json['date'],
+      startTime: TimeOfDay.fromDateTime(DateTime.parse(json['startTime'])),
+      endTime: TimeOfDay.fromDateTime(DateTime.parse(json['endTime'])),
+      numberOfPlayers: json['numberOfPayers'],
+      minSkillLevel: json['minSkillLevel'],
+      maxSkillLevel: json['maxSkillLevel'],
+      //parse to latlng
+      matchLocation: json['matchLocation'],
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'date' : date.toString(),
+      'startTime' : startTime.toString(),
+      'endTime' :endTime.toString(),
+      'numberOfPLayers' : numberOfPlayers,
+      'minSkillLevel' : minSkillLevel,
+      'maxSkillLevel' : maxSkillLevel,
+      'matchLocation' : matchLocation,
+    };
+  }
 }
