e001e33e8204b246efb7c23bdfa468b6215c3321
Ruben <rubenchambav@gmail.com>
2021-06-02 10:45:51 +0200

    Fixed Sign UP 1 and 2

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/Buttons.dart';
+import 'package:frontend/Match.dart';
 import 'package:frontend/Plannedmatches.dart';
 import 'package:frontend/Settings.dart';
 import 'package:frontend/Profile.dart';
@@ -9,6 +10,7 @@ import 'package:frontend/UserPreferences.dart';
 import 'package:frontend/CreateMatchDialog.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_webservice/places.dart';
+import 'MatchInfo.dart';
 import 'startscreen.dart';
 import 'User.dart';
 
@@ -142,11 +144,12 @@ class _HomeScreenState extends State<HomeScreen> {
                     icon: Icon(Icons.add),
                     iconSize: 25,
                     color: Colors.white,
-                    onPressed: () {
-                      showDialog(
+                    onPressed: () async {
+                      Match match = await showDialog(
                         context: context,
                         builder: (BuildContext context) => CreateMatchDialog()
                       );
+                      _updateMarker(match);
                     },
                   ),
                 ),
@@ -158,6 +161,31 @@ class _HomeScreenState extends State<HomeScreen> {
 
       drawerEnableOpenDragGesture: true,
       );
+  }
+
+  void _handleTap(Match match) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => MatchInfo(match: match),
+    );
+  }
+
+  void _updateMarker (Match match){
+    setState(() {
+      Marker marker = Marker(
+        markerId: MarkerId(match.matchLocation.toString()),
+        draggable: false,
+        infoWindow: InfoWindow(title: 'Match!', snippet: match.matchLocation.toString()),
+        position: match.matchLocation,
+        icon: BitmapDescriptor.defaultMarkerWithHue(120.0),
+        onTap: () {
+          _handleTap(match);
+        }
+      );
+      _matchMarkers.add(marker);
+      searchTennisCourts(_center);
+      _markers.addAll(_matchMarkers);
+    });
   }
 
   void searchTennisCourts(LatLng center) async {
@@ -178,6 +206,7 @@ class _HomeScreenState extends State<HomeScreen> {
           position: LatLng(f.geometry.location.lat, f.geometry.location.lng),
         );
         _markers.add(marker);
+        _markers.addAll(_matchMarkers);
       });
     });
   }
